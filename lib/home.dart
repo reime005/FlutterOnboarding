9ce87f86788731e3a44bb1d5a0b7eb286841d45b
Marius Reimer <reime005@gmail.com>
2021-01-18 23:57:38 +0000

    --

--- lib/home.dart
+++ lib/home.dart
@@ -8,12 +8,14 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with ChangeNotifier {
   final controller = PageController();
 
   OpenPainter _painter = OpenPainter(3, 1);
   bool _lights = false;
   int _index = 0;
+
+  test() {}
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +32,9 @@ class _HomePageState extends State<HomePage> {
                     scrollDirection: Axis.horizontal,
                     controller: controller,
                     onPageChanged: (value) {
+                      print(value);
                       _painter.changeIndex(value);
+                      notifyListeners();
                       setState(() {
                         _index = value;
                       });
@@ -59,13 +63,13 @@ class _HomePageState extends State<HomePage> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            "Walk your dog $_index",
+                            "Walk your dog",
                             style: Theme.of(context).textTheme.headline1,
                             textAlign: TextAlign.center,
                           ),
                           Padding(
-                              padding:
-                                  EdgeInsets.only(top: 32, left: 16, right: 16),
+                              padding: EdgeInsets.only(
+                                  top: 32, left: 16, right: 16, bottom: 16),
                               child: Text(
                                 "Reprehenderit ea ut cupidatat proident. Mollit proident eiusmod ad ex cillum officia dolore amet. Cillum amet eiusmod occaecat non non consectetur laborum do. Cupidatat dolore enim officia mollit.",
                                 style: Theme.of(context).textTheme.bodyText1,
@@ -74,12 +78,9 @@ class _HomePageState extends State<HomePage> {
                         ],
                       )),
                       Container(
-                        width: 80,
-                        height: 80,
-                        child: CustomPaint(
-                          painter: _painter,
-                        ),
-                      ),
+                          child: CustomPaint(
+                        painter: _painter,
+                      )),
                       Row(
                         children: [
                           // Padding(
@@ -135,45 +136,52 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class OpenPainter extends CustomPainter {
+class OpenPainter extends CustomPainter with ChangeNotifier {
   final int maxIndex;
   int currentIndex = 1;
 
   void changeIndex(int index) {
     currentIndex = index + 1;
+    notifyListeners();
   }
 
   @override
   void paint(Canvas canvas, Size size) {
     var paint1 = Paint()
-      ..color = Color(0xff63aa65)
+      ..color = Colors.white
       ..style = PaintingStyle.fill;
 
-    var radius = 10.0;
+    var radius = 3.0;
+    var gap = radius * 1.75;
+    var barHeight = radius * 2;
+    var barWidth = radius * 7;
+
     var distance = 0.0;
-    var gap = 10.0;
+    var barRadius = radius * 2;
 
     for (int i = 1; i <= maxIndex; i++) {
-      if (i == currentIndex) {
-        canvas.drawRRect(
-            RRect.fromRectAndCorners(Rect.fromLTRB(distance - radius, -5, distance + radius * 3, 5),
-                bottomLeft: Radius.circular(5),
-                bottomRight: Radius.circular(5),
-                topLeft: Radius.circular(5),
-                topRight: Radius.circular(5)),
-            paint1);
+      var width = i == currentIndex ? barWidth : radius * 2;
+      var height = i == currentIndex ? barHeight : radius * 2;
 
-        distance += (radius * 2 * 2) + gap;
-        continue;
+      // simple calculation to center the canvas
+      if (i == 1) {
+        var fullLength =
+            (maxIndex - 1) * gap + (maxIndex - 1) * barRadius + barWidth;
+        canvas.translate((size.width / 2) - fullLength / 2, 0);
       }
 
-      canvas.drawCircle(Offset(distance, 0), radius, paint1);
-      distance += (radius * 2) + gap;
-    }
+      canvas.drawRRect(
+          RRect.fromRectAndCorners(
+              Rect.fromLTRB(
+                  distance, -(height / 2), distance + (width), barHeight / 2),
+              bottomLeft: Radius.circular(barRadius),
+              bottomRight: Radius.circular(barRadius),
+              topLeft: Radius.circular(barRadius),
+              topRight: Radius.circular(barRadius)),
+          paint1);
 
-    //a circle
-    // canvas.drawLine(Offset(0, 0), Offset(10.0 * _index, 0), paint1);
-    // canvas.drawCircle(Offset(24, 24), 24, paint1);
+      distance += width + gap;
+    }
   }
 
   OpenPainter(this.maxIndex, this.currentIndex) {}
